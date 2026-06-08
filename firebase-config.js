6a9b180b9ae80d9747e2aaa5965f16076e052d27
SupramXD <supramxd@users.noreply.github.com>
2026-06-08 16:39:28 +0200

    Revert "Integrate Firebase App Check using reCAPTCHA v3 on frontend"
    This reverts commit 2769a0f94b7595543c2158e1611ebae5c6541958.

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyC8srbzH_DcCYQJXe9MNOyy2OHZSaLidIo",
-    authDomain: "minara5.firebaseapp.com",
-    projectId: "minara5",
-    storageBucket: "minara5.firebasestorage.app",
-    messagingSenderId: "860405871052",
-    appId: "1:860405871052:web:2aead90773c24721f72d69"
-};
